e32f4a7e7b96278b8ce8a013e1b37df0aa8ff130
Robert Shonk <rshonkcode@gmail.com>
2025-11-15 11:35:24 -0800

    /sectors mobile styling mostly done.

--- SnpWebApp/wwwroot/js/sectorsScript.js
+++ SnpWebApp/wwwroot/js/sectorsScript.js
@@ -5,12 +5,19 @@ Script for /sectors page
 // urls
 const baseUrl = "https://localhost:7188/";
 const sectorPerfUrl = `${baseUrl}api/sectorPerformance`;
+const sectorGroupsUrl = `${baseUrl}api/sectors`;
 
 async function fetchSectorsInfo() {
     const response = await fetch(sectorPerfUrl);
     const data = await response.json();
 
-    console.log(data);
+    return data;
+}
+
+async function fetchSectorGroups() {
+    const response = await fetch(sectorGroupsUrl);
+    const data = await response.json();
+
     return data;
 }
 
@@ -20,9 +27,9 @@ function displaySectorsInfo(data) {
 
     let sectors = [];
     let moveMeans = [];
-    data.forEach(d => {
-        sectors.push(d['sectorName']);
-        moveMeans.push(d['sectorMoveMean']);
+    Object.keys(data).forEach(key => {
+        sectors.push(key);
+        moveMeans.push(data[key]);
     });
 
     new Chart(ctx, {
@@ -54,10 +61,119 @@ function displaySectorsInfo(data) {
     });
 }
 
-async function renderView() {
-    const data = await fetchSectorsInfo();
+function createTop20Tables(data, sortedKeys) {
+    const sectorStocks = document.getElementById("sectorStocks");
 
-    displaySectorsInfo(data);
+    sortedKeys.forEach(key => {
+        const sectorInfo = document.createElement("div");
+        sectorInfo.setAttribute("class", "sectorInfo");
+
+        const sectorTitle = document.createElement("div");
+        sectorTitle.setAttribute("class", "sectorTitle");
+        sectorTitle.textContent = key;
+        sectorInfo.append(sectorTitle);
+
+        // create top20 row
+        const top20Row = document.createElement("div");
+        top20Row.setAttribute("class", "row20");
+
+        let title20 = '';
+        title20 = document.createElement("div");
+        title20.setAttribute("class", "title20");
+        title20.textContent = "Top 20";
+        top20Row.append(title20);
+
+        data[key]["top20"].forEach(stock => {
+            const stock20 = document.createElement("div");
+            stock20.setAttribute("class", "stock20");
+
+            // create and append stockSymbol div to stock20 div
+            const stockSymbol = document.createElement("div");
+            stockSymbol.setAttribute("class", "stockSymbol");
+            stockSymbol.textContent = stock["symbol"];
+            stock20.append(stockSymbol);
+
+            // create stockData div
+            const stockData = document.createElement("div");
+            stockData.setAttribute("class", "stockData");
+
+            // create and append stockClose and stockMove to stockData div
+            const stockClose = document.createElement("div");
+            stockClose.setAttribute("class", "stockClose");
+            stockClose.textContent = stock["close"];
+            stockData.append(stockClose);
+
+            const stockMove = document.createElement("div");
+            stockMove.setAttribute("class", "stockMove");
+            stockMove.textContent = stock["move"].toFixed(2);
+            stockData.append(stockMove);
+
+            // append stockData to stock20 div
+            stock20.append(stockData);
+
+            // finally append stock20 to top20Row
+            top20Row.append(stock20);
+        });
+        
+        // create bot20 row
+        const bot20Row = document.createElement("div");
+        bot20Row.setAttribute("class", "row20");
+
+        title20 = document.createElement("div");
+        title20.setAttribute("class", "title20");
+        title20.textContent = "Bot 20";
+        bot20Row.append(title20);
+
+        data[key]["bot20"].forEach(stock => {
+            const stock20 = document.createElement("div");
+            stock20.setAttribute("class", "stock20");
+
+            // create and append stockSymbol div to stock20 div
+            const stockSymbol = document.createElement("div");
+            stockSymbol.setAttribute("class", "stockSymbol");
+            stockSymbol.textContent = stock["symbol"];
+            stock20.append(stockSymbol);
+
+            // create stockData div
+            const stockData = document.createElement("div");
+            stockData.setAttribute("class", "stockData");
+
+            // create and append stockClose and stockMove to stockData div
+            const stockClose = document.createElement("div");
+            stockClose.setAttribute("class", "stockClose");
+            stockClose.textContent = stock["close"];
+            stockData.append(stockClose);
+
+            const stockMove = document.createElement("div");
+            stockMove.setAttribute("class", "stockMove");
+            stockMove.textContent = stock["move"].toFixed(2);
+            stockData.append(stockMove);
+
+            // append stockData to stock20 div
+            stock20.append(stockData);
+
+            // finally append stock20 to top20Row
+            bot20Row.append(stock20);
+        });
+        const rowContainer = document.createElement("div");
+        rowContainer.setAttribute("class", "rowContainer");
+        rowContainer.append(top20Row);
+        rowContainer.append(bot20Row);
+
+        // append everything to sectorStocks div
+        sectorInfo.append(sectorTitle);
+        sectorInfo.append(rowContainer);
+
+        sectorStocks.append(sectorInfo);
+    });
+}
+
+async function renderView() {
+    const info = await fetchSectorsInfo();
+    const sectorGroups = await fetchSectorGroups();
+
+    displaySectorsInfo(info);
+    createTop20Tables(sectorGroups, Object.keys(info));
 }
 
 renderView();
